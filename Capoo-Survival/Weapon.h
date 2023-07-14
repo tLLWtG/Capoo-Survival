f23d7b2d669ce5c20db614e90a0adf3cda2b476f
wegret <72203526+wegret@users.noreply.github.com>
2023-07-14 20:46:45 +0800

    Update Weapon.h

--- Capoo-Survival/Weapon.h
+++ Capoo-Survival/Weapon.h
@@ -1,26 +1,51 @@
 #pragma once
+
+#include "stdafx.h"
 #include "VisibleGameObject.h"
 #include "Animation.h"
 
 class Weapon :public VisibleGameObject {
+protected:
+	Animator m_Animator;
+	int attackPower;
+	float attackTime;
+	float attackDuration;
+	// 暂时还没写除了攻击外的别的属性
+
 public:
-	Weapon();
+	enum WeaponState {
+		Attacking, Holding
+	};
+	WeaponState weaponstate;
 
-	Animator m_animator;
-	
-	void Load(std::string filename) {
-		_texture = AssetManager::GetTexture(filename);
-		_sprite.setTexture(_texture);
-		_isLoaded = true;
-	}
-
-	void Draw(sf::RenderWindow& window) override {
+	Weapon() : weaponstate(Holding),m_Animator(GetSprite()) {
 
 	}
-	void Fire() {
-
+	virtual void Update(float elapsedTime) {
+		if (weaponstate == Attacking) {
+			attackTime += elapsedTime;
+			if (attackTime >= attackDuration) 
+				Hold();
+		}
+		m_Animator.Update(sf::seconds(elapsedTime));
 	}
-
-
-
+	virtual void Draw(sf::RenderWindow& window) {
+		window.draw(GetSprite());
+	}
+	virtual void Fire() {
+		if (weaponstate == Holding) {
+			weaponstate = Attacking;
+			attackTime = 0.0f;
+			m_Animator.SwitchAnimation("Attack");
+		}
+	}
+	virtual void Hold() {
+		if (weaponstate == Attacking) {
+			weaponstate = Holding;
+			m_Animator.SwitchAnimation("Hold");
+		}
+	}/*
+	bool isHit()	// 碰撞检测可能需要怪物的接口
+	// //////////////
+	*/
 };

89df6ab724d0f7eeaccbf2cb16495aa0c6e31c6c
wegret <72203526+wegret@users.noreply.github.com>
2023-07-16 22:43:53 +0800

    Update Mainmenu.cpp

--- Capoo-Survival/Mainmenu.cpp
+++ Capoo-Survival/Mainmenu.cpp
@@ -4,23 +4,18 @@
 
 
 Mainmenu::Mainmenu() : Interface() {
+	setBackground("Image/Mainmenu/background_glass.png");
+	addIconButton(sf::Vector2f(1203, 520), "Image/IconButton/setting.png", "setting");
+	addIconButton(sf::Vector2f(1203, 600), "Image/IconButton/music.png", "music");
+	addIconButton(sf::Vector2f(1208, 675), "Image/IconButton/volume.png", "volume");
+	addTextButton(sf::Vector2f(640, 260), L"START", "START");
+	addTextButton(sf::Vector2f(640, 360), L"CONTINUE", "CONTINUE");
+	addTextButton(sf::Vector2f(640, 460), L"ACHIEVEMENT", "ACHIEVEMENT");
+}
 
-}
-void Mainmenu::init() {
-	if (uninitialized) {
-		setBackground("Image/Mainmenu/background_glass.png");
-		addIconButton(sf::Vector2f(1210, 520), "Image/IconButton/setting.png", "setting");
-		addIconButton(sf::Vector2f(1203, 600), "Image/IconButton/music.png", "music");
-		addIconButton(sf::Vector2f(1208, 675), "Image/IconButton/volume.png", "volume");
-		addTextButton(sf::Vector2f(640, 260), L"START", "START");
-		addTextButton(sf::Vector2f(640, 360), L"CONTINUE", "CONTINUE");
-		addTextButton(sf::Vector2f(640, 460), L"ACHIEVEMENT", "ACHIEVEMENT");
-		uninitialized = false;
-	}
-}
 Mainmenu::MenuResult Mainmenu::show(sf::RenderWindow& window) {
 	static sf::Sprite background_sprite;
-	background_sprite.setPosition(0, 300);
+	background_sprite.setPosition(0 + getWindowStart().x, 300.0f + getWindowStart().y);
 	Animator animator(background_sprite);
 	auto& cloudAnimation = animator.CreateAnimation("clouds", "Image/Mainmenu/cloud_1285x61.png", sf::seconds(2), true);
 	cloudAnimation.AddFrames(sf::Vector2i(0, 0), sf::Vector2i(1285, 61), 30);
@@ -35,7 +30,6 @@ Mainmenu::MenuResult Mainmenu::show(sf::RenderWindow& window) {
 				std::string buttonName = getClickButtonName(getMousePosition(window));
 				if (buttonName == "START")
 					return Mainmenu::MenuResult::Playing;
-				///////////////////////////////////
 			}
 		}
 		animator.Update(clock.restart());

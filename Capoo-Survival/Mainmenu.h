2119050679744ce694751756c6ee89522980fe6d
wegret <72203526+wegret@users.noreply.github.com>
2023-07-16 22:43:28 +0800

    Update Mainmenu.h

--- Capoo-Survival/Mainmenu.h
+++ Capoo-Survival/Mainmenu.h
@@ -3,24 +3,21 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
-#include "Game.h"
 #include "Interface.h"
 #include "Animator.h"
 
 class Mainmenu : public Interface {
 public:
-	
+
 	enum MenuResult {
 		Playing, Exiting
 	};
 
 	Mainmenu();
-	void init();
 	MenuResult show(sf::RenderWindow& window);
 	void click(sf::RenderWindow& window);
 
 private:
-	bool uninitialized = true;
 };
 
 #endif

8a85c2efb6be63c86174c44510c216e19f0d059b
wegret <72203526+wegret@users.noreply.github.com>
2023-07-19 18:49:32 +0800

    Update Sword.cpp

--- Capoo-Survival/Sword.cpp
+++ Capoo-Survival/Sword.cpp
@@ -1,2 +1,17 @@
 #include "stdafx.h"
 #include "Sword.h"
+
+
+Sword::Sword() : Weapon() {
+    attackPower = 10;
+    attackDuration = 0.5f;
+
+    GetSprite().setOrigin(13, 89);
+    startAngle = 37;
+
+    auto& holdAnimation = m_Animator.CreateAnimation("Hold", "Image/Weapon/sword_hold_160x160.png", sf::seconds(0.5), true);
+    holdAnimation.AddFrames(sf::Vector2i(0, 0), sf::Vector2i(160, 160), 30);
+    auto& attackAnimation = m_Animator.CreateAnimation("Attack", "Image/Weapon/sword_attack_160x160.png", sf::seconds(0.5), true);
+    attackAnimation.AddFrames(sf::Vector2i(0, 0), sf::Vector2i(160, 160), 30);
+    m_Animator.SwitchAnimation("Hold");
+}
